638e27ad7ae5358e787d7ee9e57c0b03847a1c10
Jacek <jpnski@protonmail.ch>
2020-11-06 22:50:51 -0800

    update keymap

--- keyboards/dactyl_manuform/keymaps/defaults/keymap.c
+++ keyboards/dactyl_manuform/keymaps/defaults/keymap.c
@@ -5,7 +5,7 @@
 // qwerty ansi-style base layer
 // layer1 = numbers, operators, media
 // layer2 = function keys, symbols
-// layer3 = symbols, nav, reset
+// layer3 = brackets/tilde, nav, reset
 
 
 #define base 0
@@ -29,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
   [base] = LAYOUT_4x12( 
-    KC_ESC,   KC_Q,    KC_W,     KC_E,          KC_R,          KC_T,   KC_Y,          KC_U,           KC_I,     KC_O,     KC_P,     KC_BSPC,\
-    KC_TAB,   KC_A,    KC_S,     KC_D,          KC_F,          KC_G,   KC_H,          KC_J,           KC_K,     KC_L,     KC_SCLN,  KC_QUOT,\
-    KC_LSFT,  KC_Z,    KC_X,     KC_C,          KC_V,          KC_B,   KC_N,          KC_M,           KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT ,\
-    KC_LCTL,  KC_LGUI, KC_LALT,  LT(1,KC_TRNS), LT(2,KC_TRNS), KC_SPC, LT(1,KC_TRNS), LT(2,KC_TRNS),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT \
+    KC_TAB,   KC_Q,    KC_W,     KC_E,      KC_R,    KC_T,                           KC_Y,           KC_U,      KC_I,     KC_O,     KC_P,     KC_BSPC,\
+    KC_SFT,   KC_A,    KC_S,     KC_D,      KC_F,    KC_G,                           KC_H,           KC_J,      KC_K,     KC_L,     KC_SCLN,  KC_QUOT,\
+    KC_LCTL,  KC_Z,    KC_X,     KC_C,      KC_V,    KC_B,                           KC_N,           KC_M,      KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT ,\
+                                            KC_LGUI, KC_LALT, KC_SPC, LT(1,KC_TRNS), LT(2,KC_TRNS),  LT(3,KC_TRNS) \
 ),
 
 /* layer 1
@@ -48,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
   
   [layer1] = LAYOUT_4x12( 
-    KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL ,\
-    KC_CAPS,  KC_PGUP,  KC_TRNS,  KC_TRNS,  KC_END,   KC_UNDS, KC_EQL,   KC_MINS,  KC_PLUS,  KC_LPRN,  KC_RPRN,  KC_BSLS,\
-    KC_CAPS,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS \
+    KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL ,\
+    KC_CAPS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_UNDS,                      KC_EQL,   KC_MINS,  KC_PLUS,  KC_LPRN,  KC_RPRN,  KC_BSLS,\
+    KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,\
+                                            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS \
 ),
   
 /* layer 2
@@ -68,10 +68,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
   
   [layer2] = LAYOUT_4x12( 
-    KC_TILD,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11 ,\
-    KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LBRC,  KC_RBRC,  KC_PIPE,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  BL_DEC,   BL_INC,   BL_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LCBR,  KC_RCBR,  KC_TRNS,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RESET   \
+    KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11 ,\
+    KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                      KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LBRC,  KC_RBRC,  KC_PIPE,\
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,\
+                                            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS \
 ),
 
 /* layer 3
@@ -88,10 +88,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
   
   [layer3] = LAYOUT_4x12( 
-    KC_TILD,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11 ,\
-    KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LBRC,  KC_RBRC,  KC_PIPE,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  BL_DEC,   BL_INC,   BL_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LCBR,  KC_RCBR,  KC_TRNS,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RESET   \
+    KC_TILD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,\
+    KC_TRNS,  KC_PGDN,  KC_PGUP,  KC_HOME,  KC_END,   KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LCBR,  KC_RCBR,  KC_TRNS,\
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_RST, \
+                                            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS \
 )
 
 };
